89b23e2ee30cfe02ca6dc1644a9baf3f49837f15
benjaminwan <benjaminwan@163.com>
2021-01-04 16:40:47 +0800

    boxPoint坐标值减去padding值，得到实际坐标值

--- OcrLibrary/src/main/cpp/src/OcrLite.cpp
+++ OcrLibrary/src/main/cpp/src/OcrLite.cpp
@@ -11,12 +11,15 @@ void OcrLite::init(JNIEnv *env, jobject assetManager, int numOfThread) {
         LOGE(" %s", "AAssetManager==NULL");
     }
 
+    Logger("--- Init DbNet ---\n");
     dbNet.setNumThread(numOfThread);
     bool retDbNet = dbNet.initModel(mgr);
 
+    Logger("--- Init AngleNet ---\n");
     angleNet.setNumThread(numOfThread);
     bool retAngleNet = angleNet.initModel(mgr);
 
+    Logger("--- Init CrnnNet ---\n");
     crnnNet.setNumThread(numOfThread);
     bool retCrnnNet = crnnNet.initModel(mgr);
 
@@ -94,8 +97,7 @@ OcrResult OcrLite::detect(cv::Mat &src, cv::Rect &originRect, ScaleParam &scale,
 
     //Log Angles
     for (int i = 0; i < angles.size(); ++i) {
-        Logger("angle[%d][index(%d), score(%f), time(%fms)]", i, angles[i].index, angles[i].score,
-               angles[i].time);
+        Logger("angle[%d][index(%d), score(%f), time(%fms)]", i, angles[i].index, angles[i].score, angles[i].time);
     }
 
     //Rotate partImgs
@@ -124,10 +126,14 @@ OcrResult OcrLite::detect(cv::Mat &src, cv::Rect &originRect, ScaleParam &scale,
 
     std::vector<TextBlock> textBlocks;
     for (int i = 0; i < textLines.size(); ++i) {
-        TextBlock textBlock{textBoxes[i].boxPoint, textBoxes[i].score, angles[i].index,
-                            angles[i].score,
-                            angles[i].time, textLines[i].text, textLines[i].charScores,
-                            textLines[i].time,
+        std::vector<cv::Point> boxPoint = std::vector<cv::Point>(4);
+        int padding = originRect.x;//padding conversion
+        boxPoint[0] = cv::Point(textBoxes[i].boxPoint[0].x - padding, textBoxes[i].boxPoint[0].y - padding);
+        boxPoint[1] = cv::Point(textBoxes[i].boxPoint[1].x - padding, textBoxes[i].boxPoint[1].y - padding);
+        boxPoint[2] = cv::Point(textBoxes[i].boxPoint[2].x - padding, textBoxes[i].boxPoint[2].y - padding);
+        boxPoint[3] = cv::Point(textBoxes[i].boxPoint[3].x - padding, textBoxes[i].boxPoint[3].y - padding);
+        TextBlock textBlock{boxPoint, textBoxes[i].score, angles[i].index, angles[i].score,
+                            angles[i].time, textLines[i].text, textLines[i].charScores, textLines[i].time,
                             angles[i].time + textLines[i].time};
         textBlocks.emplace_back(textBlock);
     }
